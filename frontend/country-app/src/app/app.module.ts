6e8509985cabe1294f19ac3bb83818a2e69e2680
Roger <rogergibert8@gmail.com>
2023-05-25 13:46:42 +0200

    LandingPage, NavigationBar, Footer

--- frontend/country-app/src/app/app.module.ts
+++ frontend/country-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    AboutComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
